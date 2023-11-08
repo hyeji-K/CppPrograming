e36347da71599d8a48aad320c390b13c1bf13435
Khyeji <khjji0502@gmail.com>
2023-11-08 20:53:56 +0900

    C++ programming Init

---
+++ CppProgramming/CppProgramming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppProgramming
+//
+//  Created by heyji on 2023/11/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
